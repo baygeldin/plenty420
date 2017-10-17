ff28fc2e2e1dfda00e624defa8d8a5c6b8d305a9
Alexander Baygeldin <a.baygeldin@gmail.com>
2017-10-17 22:20:19 +0300

    Improve UX a bit.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const FileSync = require('lowdb/adapters/FileSync')
 const TeleBot = require('telebot')
 
 const home = 'http://www.spbbong.com'
+const sale = `${home}/rabotaem_v_minus`
 const token = '470996714:AAGwXcJV8-qVyY6nj4w1q7_taWZSi7nMv3M'
 const passphrase = 'snoopdogg'
 const interval = 10000
@@ -21,7 +22,9 @@ db.defaults({ users: [], last: '' }).write()
 
 process.on('uncaughtException', (err) => {
   logger.error(err.toString())
-  db.get('users').value().forEach((u) => bot.sendMessage(u.id, "Holy moly guacamole! Something went very wrong! You betta check out them logs. I'm outta here!").then(() => process.exit()).catch((e) => logger.error(err.toString())))
+  let text = "Holy moly guacamole! Something went very wrong! You betta check out them logs. I'm outta here!"
+  db.get('users').value().forEach((u) => bot.sendMessage(u.id, text)
+    .then(() => process.exit()).catch((e) =>  logger.error(err.toString())))
 })
 
 let queue = []
@@ -55,16 +58,19 @@ bot.on('text', (msg) => {
       msg.reply.text("Try harder, you'll get there eventually.")
     }
   } else if (db.get('users').find({ id: msg.from.id }).value()) {
-    msg.reply.text("Chill out, dawg. I'll notify you when there's somthing worth looking into. Peace!")
+    if (msg.text === '/link') {
+      msg.reply.text(`Here you go, pal: ${sale}`)
+    } else {
+      msg.reply.text("Chill out, dawg. I'll notify you when there's somthing worth looking into. Peace!")
+    }
   } else {
     msg.reply.text("Join da club first. Type /start to get baked.")
   }
 })
 
 setInterval(function(){
-  needle.get(`${home}/rabotaem_v_minus`, (err, res) => {
+  needle.get(sale, (err, res) => {
     if (!err && res.statusCode == 200) {
-      //let $ = cheerio.load(iconv.convert(html))
       let $ = cheerio.load(res.body)
       let deal = $('.vminus').first()
       let info = deal.find('.info2')
@@ -72,11 +78,12 @@ setInterval(function(){
       let name = info.find('.name').text()
       let description = info.find('p').text()
       let image = info.find('img').attr('src')
-      let oldPrice = meta.find('.oldpricediv').text()
-      let newPrice = meta.find('.pricediv').text()
+      let oldPrice = parseInt(meta.find('.oldpricediv').text(), 10)
+      let newPrice = parseInt(meta.find('.pricediv').text(), 10)
 
       if (db.get('last').value() !== name) {
-        caption = `Check out the new deal! ${name} for ${newPrice} instead of ${oldPrice}:\n\n${description}`
+        let text = `${name} (${oldPrice}₽ - ${oldPrice - newPrice}₽ = ${newPrice}₽!):\n\n${description}`
+        let caption = text.substring(0, 197) + '...'
         db.get('users').value().forEach((u) => {
           if (image) {
             bot.sendPhoto(u.id, `${home}${image}`, { caption })
